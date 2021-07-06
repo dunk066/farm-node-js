23b62d007c899d78fcbb34f5718b8ccfebcc7be7
Kush Mishra <52556969+dunk066@users.noreply.github.com>
2021-07-06 15:58:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+// SERVER
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req,res) =>{
+        const { query , pathname} = url.parse(req.url,true);
+
+        // overview page
+        
+        if(pathname === '/'  || pathname === '/overview'){
+            res.writeHead(200,{'Content-type':'text/html'});
+            const cardsHtml = dataObj.map(el => replaceTemplate(tempCard,el)).join('');
+            const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
+            res.end(output);
+        }
+        
+        // product page
+    
+        else if(pathname === '/product'){
+            res.writeHead(200,{'Content-type':'text/html'});
+            const product = dataObj[query.id];
+            const output = replaceTemplate(tempProduct,product);
+            res.end(output);
+        }
+        
+        // api
+    
+        else if(pathname === '/api'){
+            res.writeHead(200,{'Content-type':'application/json'});
+            res.end(data);
+        }
+        
+        // not found
+    
+        else{
+            res.end('Not found');
+        }
+    });
+  
+  server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+  });
+
